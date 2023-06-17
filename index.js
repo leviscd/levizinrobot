20bf17650629e524be709271265168cbfd85e499
Levi Santana <130956471+leviscd@users.noreply.github.com>
2023-06-17 16:17:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,58 +8,51 @@ const client = await connect()
  client.ev.on('messages.upsert', async ({ messages }) => {
     const body = messages[0]
   const jid = body.key.remoteJid
-  const owner = ["seunumero@s.whatsapp.net"]
+  const owner = ["idaqui@s.whatsapp.net"]
   const messageId = body.key.id
   const fromMe = body.fromMe
   const isGroup = body.key.remoteJid.endsWith('@g.us') 
   const prefix = ['/']
   const nome = body.pushName || "Usuário (a)"
-  const textmsg = body.message.conversation.toLowerCase();
-
+  
+ console.log(body) //opcional
+  
   if (isGroup) {
-    return; // Não responder a mensagens de grupos, para responder so tirar esse if.
+    return; // Não responder a mensagens de grupos
   }
- 
+
   function reply(text) {
-    // Enviar uma mensagem para o jid, usando o text e o id do body como quoted
+    // Enviar uma mensagem para o jid, usando o text e o id do body como quoted no caso isso seria p tipo que enviar de forma simples marcando a mensagem
     client.sendMessage(jid, {text: text}, {quoted: body})
   }
   // caso contrário, prosseguir com o tratamento do comando
   const command = body.message.conversation.split(prefix)[1]
   const textuser = body.message.conversation.toLowerCase();
-    
 
-  if (
-    textuser.includes("oi") ||
-    textuser.includes("ou") ||
-    textuser.includes("opa") ||
-    textuser.includes("olá") ||
-    textuser.includes("tudo bem") ||
-    textuser.includes("tudo bem?") ||
-    textuser.includes("bom") ||
-    textuser.includes("boa") ||
-    textuser.includes("dia") ||
-    textuser.includes("noite") ||
-    textuser.includes("tarde")
-  ) {
-    async function sendMessages() {
-      await client.sendMessage(jid, {
-        react: {
-          text: "💻",
-          key: body.key
-        }
-      });
-    }
-    client.sendMessage(jid, { 
-          image: fs.readFileSync("menu.jpeg"), 
-          caption: `colocar uma mensagem aqui`
-sendMessages();
+
+if (/^(oi|ou|opa|olá|tudo bem\??|bom|boa|dia|noite|tarde)$/.test(textuser)) {
+  async function sendMessages() {
+    await client.sendMessage(jid, {
+      react: {
+        text: "💻",
+        key: body.key
+      }
+    });
   }
+
+  client.sendMessage(jid, {
+    image: fs.readFileSync("menu.jpeg"), //opcional
+    caption: `texto p resposta desses conteudos acima`
+  }, { quoted: body });
+
+  sendMessages();
+}
+
   switch (command) {
-   
     case "help":
       case "menu":
         case "comandos":
+
      // função asincrona pra nao dar erro ao enviar
   
       async function sendMessages() {
@@ -73,29 +66,33 @@ sendMessages();
         client.sendMessage(
           jid, 
           { 
-              text: ` Olá, ${nome}!
+              image: fs.readFileSync("menu.jpeg"), 
+              caption: ` Olá, ${nome}!
 
- ╭━⪩ MENU DE COMANDOS ⪨━
- ▢ /ping - Checa o Ping.
- ▢ /menu - Checa o Menu.
- ╰━━─「🚀」─━━`
+╭━━━━━━━━━━━━⪩  MENU DE COMANDOS ⪨━━━━━━━━━━━━╮
+│
+│🌟 /ping - Checa o Ping.
+│🍽️ /menu - Checa o Menu.
+╰━━━━━━━━━━━━━╯`
           }, {quoted: body}
       )
       }
       
       sendMessages();
       break;
-   
-   case "ping":
-        reply("Pong")
-    break
-
+    
+    
+    case 'divulgar':
+      const args = textuser.split(" "); // separa a conversa pelo espaço
+      const texto = args[1]; // pega a mensagem que vem depois do comando
+      client.sendMessage("id@g.us", {text: texto}); // envia a mensagem para o contato
+      break;
     // Adicionar mais cases para outros comandos
     default:
       break;
   }
- 
     })
 }
 
 start();
+
